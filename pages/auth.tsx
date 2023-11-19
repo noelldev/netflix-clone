6f9ccba0f31026f17fd74618d5443a9f5b553361
kodrr <ntlauigan@gmail.com>
2023-11-19 20:43:41 +0800

    Setup prisma, Login change

--- pages/auth.tsx
+++ pages/auth.tsx
@@ -1,11 +1,16 @@
 import Input from '@/components/Input';
-import Image from 'next/image';
-import { useState } from 'react';
+import { useCallback, useState } from 'react';
 
 const Auth = () => {
   const [email, setEmail] = useState('');
   const [name, setName] = useState('');
   const [password, setPassword] = useState('');
+  const [variant, setVariant] = useState('login');
+  const toggleVariant = useCallback(() => {
+    setVariant((currentVariant) =>
+      currentVariant === 'login' ? 'register' : 'login'
+    );
+  }, []);
 
   return (
     <div className="relative h-full w-full bg-[url('/images/hero.jpg')] bg-no-repeat bg-center bg-fixed bg-cover">
@@ -15,15 +20,19 @@ const Auth = () => {
         </nav>
         <div className="flex justify-center">
           <div className="bg-black bg-opacity-70 px-16 py-16 self-center mt-2 lg:w-2/5 lg:max-w-md rounded-md w-full">
-            <h2 className="text-white text-4xl mb-8 font-semibold">Sign in</h2>
+            <h2 className="text-white text-4xl mb-8 font-semibold">
+              {variant === 'login' ? 'Sign in' : 'Register'}
+            </h2>
             <div className="flex flex-col gap-4">
-              <Input
-                id="name"
-                type="text"
-                label="Username"
-                value={name}
-                onChange={(e: any) => setName(e.target.value)}
-              />
+              {variant === 'register' && (
+                <Input
+                  id="name"
+                  type="text"
+                  label="Username"
+                  value={name}
+                  onChange={(e: any) => setName(e.target.value)}
+                />
+              )}
               <Input
                 id="email"
                 type="email"
@@ -39,13 +48,10 @@ const Auth = () => {
                 onChange={(e: any) => setPassword(e.target.value)}
               />
             </div>
-            {/* <button
-              onClick={variant === 'login' ? login : register}
-              className="bg-red-600 py-3 text-white rounded-md w-full mt-10 hover:bg-red-700 transition"
-            >
+            <button className="bg-red-600 py-3 text-white rounded-md w-full mt-10 hover:bg-red-700 transition">
               {variant === 'login' ? 'Login' : 'Sign up'}
             </button>
-            <div className="flex flex-row items-center gap-4 mt-8 justify-center">
+            {/* <div className="flex flex-row items-center gap-4 mt-8 justify-center">
               <div
                 onClick={() => signIn('google', { callbackUrl: '/profiles' })}
                 className="w-10 h-10 bg-white rounded-full flex items-center justify-center cursor-pointer hover:opacity-80 transition"
@@ -58,7 +64,7 @@ const Auth = () => {
               >
                 <FaGithub size={32} />
               </div>
-            </div>
+            </div> */}
             <p className="text-neutral-500 mt-12">
               {variant === 'login'
                 ? 'First time using Netflix?'
@@ -69,8 +75,7 @@ const Auth = () => {
               >
                 {variant === 'login' ? 'Create an account' : 'Login'}
               </span>
-              .
-            </p> */}
+            </p>
           </div>
         </div>
       </div>
